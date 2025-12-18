2714f253e808131fda345ea4d263153a43c771d4
Gan Teck Ann <ganteckann582006@gmail.com>
2025-12-18 18:12:39 +0800

    init

---
+++ _template_.php
@@ -0,0 +1,27 @@
+<?php
+    // include("");
+
+    // ... retrieve data (sql)
+
+    // ... isset($_POST[""]) - backend logic
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        // ... display data
+    ?>
+    <button></button>
+</body>
+</html>
+
+<?php
+    mysqli_close($conn);
+?>
---
+++ pages/committee/index.php
@@ -0,0 +1,1 @@
+// committe home page
