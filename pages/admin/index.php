ace7d24a3886e8c59be7e4dc80277328b95ae0bd
Gan Teck Ann <ganteckann582006@gmail.com>
2026-01-01 17:06:51 +0800

    item graph analysis (final)

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -1,7 +1,7 @@
 <?php
     include("../../conn.php");
 
-    // user data (total users, this month register user, last month register user)
+    // card data
     $sql_users = "SELECT 
                     -- total users
                     COUNT(*) as total_users,
@@ -58,11 +58,89 @@
     $data_events = mysqli_fetch_assoc($result_events);
     $events = array($data_events["total_events"], $data_events["this_month"], $data_events["last_month"]);
 
-
     // * testing data Here
     // $users = array(220, 0, 100);
     // $trees = array(270, 150, 120);
     // $events = array(12, 1, 10);
+?>
+
+
+<?php
+    // item line graph data
+    // latest date of both merchandise and tree table
+    $date_target_table = array("merchandise_purchase_history", "tree_adoption_history");
+    $date_target_column = array("merchandise_purchase_date", "tree_adoption_date");
+    $latest_date_result = array(null, null);
+
+    for ($i = 0; $i < 2; $i++) {
+        $sql= "SELECT MAX($date_target_column[$i]) as latest_date
+               FROM $date_target_table[$i]";
+
+        $result = mysqli_query($conn, $sql);
+        $data = mysqli_fetch_assoc($result);
+        $latest_date_result[$i] = $data["latest_date"];
+    }
+
+    // compare which date is the latest one, if both are null then default is curren month
+    $latestDateTime = time();
+
+    $merchDate = $latest_date_result[0];
+    $treeDate = $latest_date_result[1];
+    
+    if ($merchDate || $treeDate) {
+        if ($merchDate > $treeDate) {
+            $latestDateTime = strtotime($merchDate);
+        }
+        else {
+            $latestDateTime = strtotime($treeDate);
+        }
+    }
+
+    // store the previous 4 months from the latest date (total: 5 months) 
+    // use to query the quantity for each month and display it in the graph (x-axis)
+    $monthsQueryKeys = [];   // for query purpose ("Dec", "Jan", ...)
+    $monthsLabels = []; // for graph to display ("2025-12", "2026-01", ...)
+
+    for ($i = 4; $i >= 0; $i--) {
+        $monthsQueryKeys[]   = date('Y-m', strtotime("-$i months", $latestDateTime));
+        $monthsLabels[] = date('M', strtotime("-$i months", $latestDateTime));
+    }
+
+    // function for query the total record for each month
+    function countTotal($conn, $table, $column, $monthsQueryKeys) {
+        // make the $data as key-value type, then assign key for the $data
+        $data = array_fill_keys($monthsQueryKeys, 0);
+
+        $firstMonth = $monthsQueryKeys[0];
+        $lastMonth = $monthsQueryKeys[count($monthsQueryKeys) - 1];
+
+        $sql = "SELECT DATE_FORMAT($column, '%Y-%m') as monthsKey,
+                COUNT(*) as total
+                FROM $table
+                WHERE DATE_FORMAT($column, '%Y-%m') BETWEEN '$firstMonth' AND '$lastMonth'
+                GROUP BY monthsKey";
+        
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                // insert data based on key, then make the data as integer type
+                $data[$row["monthsKey"]] = (int)$row["total"];
+            }
+        }
+
+        // only return the values
+        return array_values($data);
+    }
+
+    // now call the countTotal function and find out the data for merchandise, tree and all
+    $merchandiseData = countTotal($conn, "merchandise_purchase_history", "merchandise_purchase_date", $monthsQueryKeys);
+    $treeData = countTotal($conn, "tree_adoption_history", "tree_adoption_date", $monthsQueryKeys);
+
+    $allData = array();
+    for ($i = 0; $i < 5; $i++) {
+        $allData[$i] = $merchandiseData[$i] + $treeData[$i];
+    }
 ?>
 
 <!DOCTYPE html>
@@ -84,82 +162,111 @@
             <h1>Welcome back! 👋</h1>
             <h2 class="page-subTitle">Monitor users, events, and system performance</h2>
 
-            <div class="data-container">
-                <?php
-                    $title = array("Total Users", "Total Trees", "Total Events");
-                    $all_data = array($users, $trees, $events);
-                    $percentage = 0;
-                    $status; $icon; $total; $this_month; $color;
-
-                    for($i = 0; $i < 3; $i++) {
-                        $status = "none"; 
-                        $icon = "";       
+            <div class="flex-container">
+                <div class="data-container">
+                    <?php
+                        $title = array("Total Users", "Total Trees", "Total Events");
+                        $all_data = array($users, $trees, $events);
                         $percentage = 0;
-                        
-                        $target_data = $all_data[$i];
-                        $total = $target_data[0];
-                        $this_month = $target_data[1];
+                        $status; $icon; $total; $this_month; $color;
 
-                        // index2 = last_month data
-                        if ($target_data[1] == 0) {
+                        for($i = 0; $i < 3; $i++) {
+                            $status = "none"; 
+                            $icon = "";       
                             $percentage = 0;
-                        }
-                        elseif ($target_data[2] > 0) {
-                            // ((this_month - last_month) / last_month) * 100
-                            $percentage = (($target_data[1] - $target_data[2]) / $target_data[2]) * 100;
-                            $status = "up";
+                            
+                            $target_data = $all_data[$i];
+                            $total = $target_data[0];
+                            $this_month = $target_data[1];
 
-                            // dont want negative number, alr use icon to represent, so make it absolute
-                            if ($percentage < 0) {
-                                $status = "down";
-                                $percentage = abs($percentage);
+                            // index2 = last_month data
+                            if ($target_data[1] == 0) {
+                                $percentage = 0;
                             }
-                        }
-                        // make sure this_month must have number, else just take default number, which is "0"
-                        elseif ($target_data[1] > 0) {
-                            $percentage = 100;
-                            $status = "up";
-                        }
-                        else {
-                            $percentage = 0;
-                        }
+                            elseif ($target_data[2] > 0) {
+                                // ((this_month - last_month) / last_month) * 100
+                                $percentage = (($target_data[1] - $target_data[2]) / $target_data[2]) * 100;
+                                $status = "up";
 
-                        // fix the deccimal number of percentage
-                        $percentage = number_format($percentage, 1);
+                                // dont want negative number, alr use icon to represent, so make it absolute
+                                if ($percentage < 0) {
+                                    $status = "down";
+                                    $percentage = abs($percentage);
+                                }
+                            }
+                            // make sure this_month must have number, else just take default number, which is "0"
+                            elseif ($target_data[1] > 0) {
+                                $percentage = 100;
+                                $status = "up";
+                            }
+                            else {
+                                $percentage = 0;
+                            }
 
-                        if ($status === "up") {
-                            $icon = "fa-solid fa-arrow-trend-up trend-up";
-                            $color = "#28a745";
-                        }
-                        elseif ($status === "down") {
-                            $icon = "fa-solid fa-arrow-trend-down trend-down";
-                            $color = "#dc3545";
-                        }
-                        else {
-                            $icon = "fa-solid fa-minus trend-flat";
-                            $color = "#6c757d";
-                        }
+                            // fix the deccimal number of percentage
+                            $percentage = number_format($percentage, 1);
 
-                        echo "<div class='data-box'>
-                                <div class='data-box-title'>
-                                    <h3>$title[$i]</h3>
+                            if ($status === "up") {
+                                $icon = "fa-solid fa-arrow-trend-up trend-up";
+                                $color = "#28a745";
+                            }
+                            elseif ($status === "down") {
+                                $icon = "fa-solid fa-arrow-trend-down trend-down";
+                                $color = "#dc3545";
+                            }
+                            else {
+                                $icon = "fa-solid fa-minus trend-flat";
+                                $color = "#6c757d";
+                            }
 
-                                    <div class='percentage-box'>
-                                        <i class='$icon'></i> 
-                                        <p>$percentage %</p>
+                            echo "<div class='data-box'>
+                                    <div class='data-box-title'>
+                                        <h3>$title[$i]</h3>
+
+                                        <div class='percentage-box'>
+                                            <i class='$icon'></i> 
+                                            <p>$percentage %</p>
+                                        </div>
                                     </div>
-                                </div>
-                                <p class='increase-number' style='color: $color'>+$this_month in this month</p>
-                                <h1>$total</h1>
-                              </div>";
-                    }
-                ?>
-            </div>
+                                    <p class='increase-number' style='color: $color'>+$this_month in this month</p>
+                                    <h1>$total</h1>
+                                </div>";
+                        }
+                    ?>
+                </div>
 
+                <div class="item-chart">
+
+                    <div class="item-chart-title">
+                        <h3>Redemption Analysis</h3>
+                        <select id="itemList" name="txtItem" required>
+                            <option value="all">All</option>
+                            <option value="merchandise">Merchandises</option>
+                            <option value="tree">Tree</option>
+                        </select>
+                    </div>
+
+                    <div class="item-chart-box">
+                        <canvas id="item-lineChart"></canvas>
+                    </div>
+                </div>
+            </div>
         </main>
 
         <?php include("footer.php"); ?>
 
+        <!-- pass data to JS file -->
+        <script>
+            // parse PHP array to JS array
+            const lineGraph_labels = <?php echo json_encode($monthsLabels); ?>;
+            const lineGraph_merchandiseData = <?php echo json_encode($merchandiseData); ?>;
+            const lineGraph_treeData = <?php echo json_encode($treeData); ?>;
+            const lineGraph_allData = <?php echo json_encode($allData); ?>;
+            
+            console.info("Line Graph Data Successful Passed to JS")
+        </script>
+
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <script src="../../scripts/admin.js"></script>
     </body>
 </html>
--- scripts/admin.js
+++ scripts/admin.js
@@ -41,3 +41,83 @@ navAnchorTag.forEach(link => {
         link.classList.add("active");
     }
 });
+
+
+// item line chart
+/*
+    PHP alr passed the data:
+        const lineGraph_labels
+        const lineGraph_merchandiseData
+        const lineGraph_treeData
+        const lineGraph_allData
+*/
+// execute directly after the HTML document structure is loaded
+document.addEventListener("DOMContentLoaded", () => {
+    const chart = document.getElementById("item-lineChart");
+    const itemSelector = document.getElementById("itemList");
+
+    // debug purpose
+    // if (!chart) return
+
+    // initialize graph
+    let itemLineChart = new Chart(chart, {
+        type: "line",
+        data: {
+            // assign lineGraph_labels (from PHP) to labels
+            labels: lineGraph_labels,
+            datasets: [{
+                label: "Redemption Quantity",
+                // assign lineGraph_allData (from PHP) to data
+                data: lineGraph_allData,
+                borderColor: "#194a7a",
+                backgroundColor: "rgba(25, 74, 122, 0.1)",
+                borderWidth: 2, // bold level of the curve line
+                tension: 0.4, // make the curve more smooth
+                fill: true
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false, // must false, so can allow CSS adjust
+            plugins: {
+                legend: { display: false } // hide the legend, alr have dropdown menu
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: { stepSize: 1 } // only integer
+                }
+            }
+        }
+    });
+
+    // listen the dropdown menu
+    itemSelector.addEventListener("change", (e) => {
+        const item = e.target.value; // get the value (all, merchandise, tree)
+
+        let newData = [];
+        let newColor = "";
+
+        if (item === "all") {
+            newData = lineGraph_allData;
+            newColor = "#194a7a";
+        }
+        else if (item === "merchandise") {
+            newData = lineGraph_merchandiseData;
+            newColor = "#3b7ddd";
+        }
+        else if (item === 'tree') {
+            newData = lineGraph_treeData;
+            newColor = "#7fb5ff";
+        }
+
+        // update the datasets data
+        itemLineChart.data.datasets[0].data = newData;
+        // update the curve color
+        itemLineChart.data.datasets[0].borderColor = newColor;
+        // update the background color
+        itemLineChart.data.datasets[0].backgroundColor = newColor + "33";
+
+        itemLineChart.update();
+    });
+});
