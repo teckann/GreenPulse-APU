be3e4cf7b83022cb7d4105a54bab7838ff7e7873
Gan Teck Ann <ganteckann582006@gmail.com>
2026-01-16 00:39:01 +0800

    security question (general)

---
+++ pages/general/securityQuestion.php
@@ -0,0 +1,8 @@
+<select name="securityQuestion" id="securityQuestion">
+    <option value="">-- Please Select --</option>
+    <option value="What is your secondary school name?">What is your secondary school name?</option>
+    <option value="What is your mother's middle name?">What is your mother's middle name?</option>
+    <option value="What is your favorite color?">What is your favorite color?</option>
+    <option value="What is your first car brand?">What is your first car brand?</option>
+    <option value="What is the city name were you born in?">What is the city name were you born in?</option>
+</select>
