50478a5e2956c727758736f069f1defb0830194c
Cynth1a <cynthiatan0121@gmail.com>
2026-01-16 19:10:10 +0800

    update

--- pages/committee/eventMain.php
+++ pages/committee/eventMain.php
@@ -71,9 +71,6 @@
                     <option>Approved</option>
                 </select>
             </div>
-
-
-
             <section class="container-event">
     <div class="content-card-event">
         
@@ -99,6 +96,9 @@
 
         <!-- Right Side: Details Section -->
         <div class="event-right-section">
+            <div>
+                
+            </div>
             
             <!-- Meta Info -->
             <div class="event-meta-row">
@@ -120,8 +120,7 @@
                 </div>
                 <span class="points-badge">500 pts</span>
                 <div class="more-section">
-                    <span>MORE</span>
-                    <i class="fas fa-arrow-right"></i>
+                    <i class="fa-solid fa-arrow-up-right-from-square"></i>
                 </div>
             </div>
 
