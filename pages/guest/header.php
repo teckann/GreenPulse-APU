9e2a9915e3dc5131ad095fbedfc7f3ad6a17c8fe
Gan Teck Ann <ganteckann582006@gmail.com>
2026-01-22 17:09:45 +0800

    guest navbar & login UI improvement

---
+++ pages/guest/header.php
@@ -0,0 +1,11 @@
+<header>
+    <img src="../../src/elements/logo_horizontal.png" alt="Logo" height="60px" class="desktop-logo">
+    <img src="../../src/elements/logo_vertical.png" alt="Logo" height="60px" width="60px" class="mobile-logo">
+
+    <nav>
+        <a href="#">Home</a>
+        <a href="aboutUs.php">About Us</a>
+        <a href="contactUs.PHP">Contact Us</a>
+        <a href="login.php">Login</a>
+    </nav>
+</header>
