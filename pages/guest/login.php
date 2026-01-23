53d062d3eeaba2988195b3da59b3a5ea3accc717
Gan Teck Ann <ganteckann582006@gmail.com>
2026-01-23 22:45:04 +0800

    update

--- pages/guest/login.php
+++ pages/guest/login.php
@@ -52,6 +52,10 @@
         <title>Login Page</title>
         
         <link rel="stylesheet" href="../../styles/guest.css">
+        <!-- style & ico -->
+        <link rel="icon" href="../../src/elements/logo_vertical.png" type="image/x-icon">
+
+        <!-- icon  -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
         <style>
