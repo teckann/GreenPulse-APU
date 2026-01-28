a4ebebab5843554e4becf5e5dfb936d6534ba731
Gan Teck Ann <ganteckann582006@gmail.com>
2026-01-29 03:44:05 +0800

    update portfolio template

---
+++ pages/guest/teckannPortfolio.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Gan Teck Ann's Portfolio</title>
+
+        <link rel="stylesheet" href="../../styles/portfolio.css">
+        <!-- style & ico -->
+        <link rel="icon" href="../../src/elements/logo_vertical.png" type="image/x-icon">
+
+        <!-- icon  -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    </head>
+    
+    <body class="flex-container">
+        <main>
+            <div class="portfolio-header">
+                <div class="header-1">
+                    <img src="../../src/avatars/U001_avatar_1768494571.png" alt="" class="avatar">
+
+                    <div class="social-media-big-container">
+                        <div class="social-media-container">
+                            <a href="https://www.linkedin.com/in/gan-teck-ann-60349b26a" target="_blank" class="social-media-icon">
+                                <i class="fa-brands fa-linkedin"></i>
+                            </a>
+
+                            <a href="https://github.com/teckann" target="_blank" class="social-media-icon">
+                                <i class="fa-brands fa-github"></i>
+                            </a>
+
+                            <a href="https://www.instagram.com" target="_blank" class="social-media-icon">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+
+                            <a href="mailto: ganteckann582006@gmail.com" class="social-media-icon">
+                                <i class="fa-solid fa-envelope"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="header-2">
+                    <h1>Gan Teck Ann</h1>
+                    <p>Software Engineering Student</p>
+                </div>
+            </div>
+
+            <div class="portfolio-details">
+                <div class="section-container">
+                    <p class="section-title">About Me</p>
+                    <p class="section-details">
+                        I'm Teck Ann, a second-year Diploma student majoring in Software Engineering at the 
+                        <strong>Asia Pacific University (APU)</strong>. During my third semester, I volunteered 
+                        with the APU CLCS front-end development team, where I gained hands-on experience with 
+                        modern technologies such as React, TypeScript, and Tailwind CSS, and collaborated closely 
+                        with other developers. I have also completed multiple assignment projects involving Java 
+                        and object-oriented programming, allowing me to apply theoretical concepts in practical 
+                        development and strengthen my problem-solving skills. Currently in my fourth semester, I 
+                        am working towards becoming a full-stack and mobile developer by building dynamic web applications 
+                        using PHP and MySQL as the backend foundation, further enhancing my experience in end-to-end system 
+                        development.
+                    </p>
+                </div>
+
+                <div class="section-container">
+                    <p class="section-title">Skills</p>
+                    
+                    <div class="tech-details">
+                        <p>Technical</p>
+
+                        <div class="tech-container">
+                            <div class="tech">
+                                <img src="../../src/icons/java.svg" class="svg-icon" alt="Java">
+                                <p>Java</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/python.svg" class="svg-icon" alt="Python">
+                                <p>Python</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/html.svg" class="svg-icon" alt="HTML5">
+                                <p>HTML5</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/css.svg" class="svg-icon" alt="CSS3">
+                                <p>CSS3</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/javascript.svg" class="svg-icon" alt="JavaScript">
+                                <p>JavaScript</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/php.svg" class="svg-icon" alt="PHP">
+                                <p>PHP</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/database.svg" class="svg-icon" alt="MySQL">
+                                <p>MySQL</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/php.svg" class="svg-icon" alt="PHP">
+                                <p>PHP</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/typescript.svg" class="svg-icon" alt="TypeScript">
+                                <p>TypeScript</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/react.svg" class="svg-icon" alt="React">
+                                <p>React</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/tailwindcss.svg" class="svg-icon" alt="Tailwind CSS">
+                                <p>Tailwind CSS</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="tech-details" style="margin-top: 1em;">
+                        <p>Development Tools</p>
+
+                        <div class="tech-container">
+                            <div class="tech">
+                                <img src="../../src/icons/vscode.svg" class="svg-icon" alt="VS Code">
+                                <p>VS Code</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/figma.svg" class="svg-icon" alt="Figma">
+                                <p>Figma</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/git.svg" class="svg-icon" alt="Git">
+                                <p>Git</p>
+                            </div>
+
+                            <div class="tech">
+                                <img src="../../src/icons/intellij.svg" class="svg-icon" alt="IntelliJ">
+                                <p>IntelliJ IDEA</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
