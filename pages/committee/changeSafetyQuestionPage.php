7a51e2c060f227636ac91067e42479514950f500
Gohyangee2006 <eeyang383@gmail.com>
2026-02-08 22:36:33 +0800

    update

--- pages/committee/changeSafetyQuestionPage.php
+++ pages/committee/changeSafetyQuestionPage.php
@@ -129,7 +129,7 @@
                         <select name="newSafetyQuestionSelect2" id="newSafetyQuestionSelect2" class="upText">
                             <option value="">-- Please Select --</option>
                             <option value="What is your secondary school name?">What is your secondary school name?</option>
-                            <option value="What is your mother's middle name?">What is your mother's middle name?</option>
+                            <option value="What is your mother's middle name?">What is the middle name of your mother?</option>
                             <option value="What is your favorite color?">What is your favorite color?</option>
                             <option value="What is your first car brand?">What is your first car brand?</option>
                             <option value="What is the city name were you born in?">What is the city name were you born in?</option>
